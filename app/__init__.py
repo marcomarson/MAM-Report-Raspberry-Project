61e63fe2207b0e616e6c8b81cb762fb138754c08
Marco Marson <12066726@uepg.br>
2017-11-20 22:15:07 -0200

    functionalities added

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ import bcrypt
 app = Flask(__name__, instance_relative_config=True)
 app.secret_key = 'secreto'  # Change this!
 
-app.config['MONGO_DBNAME']= 'Raspberry'
-app.config['MONGO_URI']= "mongodb://marcomarson:naruto18@raspberry-shard-00-00-h82fm.mongodb.net:27017,raspberry-shard-00-01-h82fm.mongodb.net:27017,raspberry-shard-00-02-h82fm.mongodb.net:27017/raspberry?ssl=true&replicaSet=Raspberry-shard-0&authSource=admin"
+app.config['MONGO_DBNAME']= 'projeto'
+app.config['MONGO_URI']= "mongodb://marcomarson:naruto18@raspberry-shard-00-00-h82fm.mongodb.net:27017,raspberry-shard-00-01-h82fm.mongodb.net:27017,raspberry-shard-00-02-h82fm.mongodb.net:27017/projeto?ssl=true&replicaSet=Raspberry-shard-0&authSource=admin"
 
 
 mongo = PyMongo(app)
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 from __future__ import print_function # In python 2.7
 import sys
 from app import app
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, url_for, request, session, redirect,send_file
 from flask_pymongo import PyMongo
 import pdfkit
 import bcrypt
@@ -14,97 +14,87 @@ from bson.objectid import ObjectId
 import gridfs
 import base64
 import datetime
-def trocames(x):
-    if x == 'Jun':
-        return 6
-    elif x =='Jul':
-        return 7
-    elif x =='Aug':
-        return 8
-    elif x =='Sep':
-        return 9
-    elif x =='Oct':
-        return 10
-    elif x =='Nov':
-        return 11
-    elif x =='Dec':
-        return 12
-
-
-
-# # @app.route('/')
-# # def index():
-# #     return render_template("index.html")
+import gridfs
+from PIL import Image
+from io import StringIO
+from scipy.misc import toimage
+# def trocames(x):
+#     if x == 'Jun':
+#         return 6
+#     elif x =='Jul':
+#         return 7
+#     elif x =='Aug':
+#         return 8
+#     elif x =='Sep':
+#         return 9
+#     elif x =='Oct':
+#         return 10
+#     elif x =='Nov':
+#         return 11
+#     elif x =='Dec':
+#         return 12
 #
-# @app.route('/')
-# def about():
-#     return render_template("about.html")
+#
+
+@app.route('/faq')
+def faq():
+    return render_template("faq.html")
+
+@app.route('/contato')
+def contato():
+    return render_template("contato.html")
+
+@app.route('/photos', methods=['POST','GET'])
+def photos():
+    if 'username' in session:
+        if request.method == 'POST':
+                print("Passou", file=sys.stderr)
+                datefrom=request.form['data_12'].split('/')
+                dateto=request.form['data_13'].split('/')
+                uri = "mongodb://marcomarson:naruto18@raspberry-shard-00-00-h82fm.mongodb.net:27017,raspberry-shard-00-01-h82fm.mongodb.net:27017,raspberry-shard-00-02-h82fm.mongodb.net:27017/projeto?ssl=true&replicaSet=Raspberry-shard-0&authSource=admin"
+                client = MongoClient(uri)
+                db = client.data
+                collection = db.fs.files
+                ap=request.form['ap']
+                cur= collection.find({apartamento: ap})
+
+                if not cur:
+                    raise Exception("mongo file does not exist! {0}".format(filename))
+                listafoto=[]
+                for image in cur:
+                    im=Image.open(image)
+                    img_io = StringIO()
+                    im.save(img_io, 'JPEG', quality=70)
+                    img_io.seek(0)
+                    listafoto.append(img_io)
+
+                return render_template("album.html", listafoto,toimage)
+
+        else:
+            return render_template("photos.html")
+    else:
+        return render_template('login.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if 'username' in session:
         if request.method == 'POST':
                 print("Passou", file=sys.stderr)
-                uri = "mongodb://marcomarson:naruto18@raspberry-shard-00-00-h82fm.mongodb.net:27017,raspberry-shard-00-01-h82fm.mongodb.net:27017,raspberry-shard-00-02-h82fm.mongodb.net:27017/raspberry?ssl=true&replicaSet=Raspberry-shard-0&authSource=admin"
-                client = MongoClient(uri)
-                db = client.test
-                collection = db.fs.files
-                cur= collection.find()
-                lista=[]
-                rfid1=[166,2,217,160,221]
-                rfid2=[54,109,54,94,51]
-                datefrom=request.form['data_12'].split('/')
-                dateto=request.form['data_13'].split('/')
+                data = mongo.db.data
+                data_found = data.find({'apartamento' : request.form['ap']})
 
-                apartamento= request.form['ap']
-                datapre= datetime.date(int(datefrom[2]), int(datefrom[1]), int(datefrom[0]))
-                datapos= datetime.date(int(dateto[2]), int(dateto[1]), int(dateto[0]))
-                #print(datapre, file=sys.stderr)
-                #print(datapos, file=sys.stderr)
-                #if()
-
-                for doc in cur:
-                    if not apartamento:
-
-                        if 'tempo_portao_aberto' in doc.keys() and 'rfid' in doc.keys():
-                            #if()
-                            if(int(doc['tempo_portao_aberto']) > 30):
-                                dataprocessing= doc['horario_abertura'].split()
-                                dataprocessing[1]= trocames(dataprocessing[1])
-                                dataprocessada= datetime.date(int(dataprocessing[4]),dataprocessing[1], int(dataprocessing[2]))
-                                print(dataprocessada, file=sys.stderr)
-                                if(doc['rfid']== rfid1 and datapre<=dataprocessada<=datapos):
-                                    doc['rfid']='Isabel'
-                                    doc['apartamento']=12
-                                    lista.append(doc)
-                                elif(doc['rfid']== rfid2 and datapre<=dataprocessada<=datapos):
-                                    doc['rfid']='Rogério'
-                                    doc['apartamento']=30
-                                    lista.append(doc)
-                    else:
-                        if 'tempo_portao_aberto' in doc.keys() and 'rfid' in doc.keys():
-                            dataprocessing= doc['horario_abertura'].split()
-                            dataprocessing[1]= trocames(dataprocessing[1])
-                            dataprocessada= datetime.date(int(dataprocessing[4]),dataprocessing[1], int(dataprocessing[2]))
-                            if(apartamento=='12'):
-
-                                if(int(doc['tempo_portao_aberto']) > 30 and doc['rfid']== rfid1):
-                                    doc['rfid']='Isabel'
-                                    doc['apartamento']=12
-                                    lista.append(doc)
-                            elif(apartamento=='30'):
-                                if(int(doc['tempo_portao_aberto']) > 30 and doc['rfid']== rfid2):
-                                    doc['rfid']='Rogério'
-                                    doc['apartamento']=30
-                                    lista.append(doc)
-                for x in lista:
-                    print(x, file=sys.stderr)
-                templatepdf=render_template('pdf.html', allvalues=lista)
-                print(type(templatepdf), file=sys.stderr)
-                config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
-                pdfkit.from_string(templatepdf,output_path='app\\static\\pdfs\\out.pdf',configuration=config)
-                print("passou", file=sys.stderr)
-                return redirect(url_for('static', filename='/'.join(['pdfs', 'out.pdf'])), code=301)
+                if data_found:
+                    print("Achou")
+                else:
+                    return 'Não foi encontrado nada'
+                # for x in lista:
+                #     print(x, file=sys.stderr)
+                # templatepdf=render_template('pdf.html', allvalues=lista)
+                # print(type(templatepdf), file=sys.stderr)
+                # config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+                # pdfkit.from_string(templatepdf,output_path='app\\static\\pdfs\\out.pdf',configuration=config)
+                # print("passou", file=sys.stderr)
+                # return redirect(url_for('static', filename='/'.join(['pdfs', 'out.pdf'])), code=301)
 
         else:
             return render_template('report.html')
@@ -121,21 +111,23 @@ def login():
         print(request.form['pass'].encode('utf-8'), file=sys.stderr)
         if  bcrypt.checkpw(request.form['pass'].encode('utf-8'), login_user['password']):
             session['username'] = request.form['username']
-            return redirect(url_for('login'))
+            return redirect(url_for('register'))
 
     return 'Invalid username/password combination'
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+
     if 'username' in session:
+
         if request.method == 'POST':
             users = mongo.db.users
             existing_user = users.find_one({'name' : request.form['username']})
 
             if existing_user is None:
-                hashpass = request.form['pass']
+                hashpass = bcrypt.hashpw(request.form['pass'].encode('utf8'), bcrypt.gensalt())
                 rfid = request.form['rfid']
-                users.insert({'name' : request.form['username'], 'apartamento' : hashpass, 'rfid': rfid, 'email' : request.form['email']})
+                users.insert({'name' : request.form['username'], 'password':hashpass,'apartamento' : request.form['ap'], 'rfid': rfid, 'email' : request.form['email']})
                 return redirect(url_for('index'))
 
             return 'That username already exists!'
@@ -143,12 +135,14 @@ def register():
         return render_template('register.html')
 
     else:
-        return render_template('index.html')
+        return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
-    session.pop('username',None)
+    if 'username' in session:
+
+        session.pop('username',None)
     return redirect(url_for('index'))
 
 @app.route('/report', methods=['POST', 'GET'])
